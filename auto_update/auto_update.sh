ab4fcb6d59088306fc1370336bbb28a8ff5ae1b0
Guo <guo@Home.lan>
2017-08-11 00:47:31 +0800

    add auto_update script by williamK

---
+++ auto_update/auto_update.sh
@@ -0,0 +1,10 @@
+echo
+echo "# Copyright (c) 2014-2017,by william_kwok"
+echo "# 本脚本仅用于个人研究与学习使用，从未用于产生任何盈利（包括“捐赠”等方式）"
+echo "# 未经许可，请勿内置于软件内发布与传播！请勿用于产生盈利活动！请遵守当地法律法规，文明上网。"
+echo "# openwrt类固件使用，包括但不限于pandorabox、LEDE、ddwrt等，Padavan系列固件慎用。"
+echo "# 本脚本需要安装wget插件"
+sleep 3
+echo -e "\e[1;36m 创建dnsmasq规则与更新脚本存放的文件夹\e[0m"
+echo
+echo -e "\e[1;36m 检测和备份当前dnsmasq配置信息\e[0m"
